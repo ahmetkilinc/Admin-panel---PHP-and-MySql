8e2abd13a8e1775ea5fa191255d648e72853b3bc
ahmetkilinc <ahmetkilinc3899@gmail.com>
2017-10-31 12:18:42 +0300

    +sign-in screen is developed. +mysql connection is made. +db has been configured.

---
+++ lostpassword.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<style>
+			
+		</style>
+	</head>
+	<body>
+		<h1>admin mailinizi giriniz, eğer mailiniz onaylı bir yöneticiye ait ise mailinize şifreniz gelecektir.</h1>
+		
+	</body>
+</html>
+
+<?php
+	
+
+?>
